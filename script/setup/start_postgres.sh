23dc21798367523c0628b469c94b9eed40a93296
Albert Yi <ayi@desk.com>
2017-10-17 17:13:27 -0700

    fix postgres startup

--- script/setup/start_postgres.sh
+++ script/setup/start_postgres.sh
@@ -4,4 +4,4 @@ DOCKER_HOST=elf
 PG_PASS=j9JpdMDoUo6lh3rt8egKIQ
 PG_DATA=/var/lib/postgresql/data
 
-docker service create --name=postgres --mount="type=bind,src=$PG_DATA,dst=$PG_DATA" --publish=5432:5432 --replicas=1 --constraint="node.hostname == $DOCKER_HOST" --env="POSTGRES_PASSWORD=$PG_PASS" r888888888/postgres:latest
+docker service create --name=postgres --mount="type=bind,src=$PG_DATA,dst=$PG_DATA" --publish=5432:5432 --replicas=1 --constraint="node.hostname == $DOCKER_HOST" --env="POSTGRES_PASSWORD=$PG_PASS" --network=danboorunet r888888888/postgres:latest
