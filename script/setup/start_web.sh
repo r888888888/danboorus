b74d728e51e419cc71935f863a06d0795f0b57ed
Albert Yi <ayi@desk.com>
2017-10-17 17:18:52 -0700

    enable unicorn

--- script/setup/start_web.sh
+++ script/setup/start_web.sh
@@ -7,4 +7,4 @@ REPLICAS=1
 
 docker secret rm danboorus_env || true
 docker secret create danboorus_env ~/config/danboorus_env
-docker service create --name=web --mount="type=bind,src=$DATA_DIR,dst=$DATA_DIR" --publish=$WEB_PORT:$WEB_PORT --replicas=1 --network=danboorunet --secret=danboorus_env --constraint="node.labels.$DOCKER_LABEL == true" --env="RAILS_ENV=production" r888888888/danboorus sleep 1d
+docker service create --name=web --mount="type=bind,src=$DATA_DIR,dst=$DATA_DIR" --publish=$WEB_PORT:$WEB_PORT --replicas=1 --network=danboorunet --secret=danboorus_env --constraint="node.labels.$DOCKER_LABEL == true" --env="RAILS_ENV=production" --detach=true r888888888/danboorus
