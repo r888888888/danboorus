1a2c92c85e7436ec56c8ec945739ae0cb5fcfd23
Albert Yi <ayi@desk.com>
2017-10-17 17:27:14 -0700

    specify log dir

--- script/setup/start_web.sh
+++ script/setup/start_web.sh
@@ -2,9 +2,10 @@
 
 DOCKER_LABEL=web
 DATA_DIR=/var/www/danbooru/shared/data
+LOG_DIR=/var/log/web
 WEB_PORT=3000
 REPLICAS=1
 
 docker secret rm danboorus_env || true
 docker secret create danboorus_env ~/config/danboorus_env
-docker service create --name=web --mount="type=bind,src=$DATA_DIR,dst=$DATA_DIR" --publish=$WEB_PORT:$WEB_PORT --replicas=1 --network=danboorunet --secret=danboorus_env --constraint="node.labels.$DOCKER_LABEL == true" --env="RAILS_ENV=production" --detach=true r888888888/danboorus
+docker service create --name=web --mount="type=bind,src=$DATA_DIR,dst=$DATA_DIR" --mount="type=bind,src=$LOG_DIR,dst=$LOG_DIR" --publish=$WEB_PORT:$WEB_PORT --replicas=1 --network=danboorunet --secret=danboorus_env --constraint="node.labels.$DOCKER_LABEL == true" --env="RAILS_ENV=production" --detach=true r888888888/danboorus
