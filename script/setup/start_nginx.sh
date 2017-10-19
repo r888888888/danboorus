1ccad6d119430525b6605526e1882e6de2dbf8c0
Albert Yi <ayi@desk.com>
2017-10-19 16:40:19 -0700

    fixes

--- script/setup/start_nginx.sh
+++ script/setup/start_nginx.sh
@@ -2,5 +2,6 @@
 
 DOCKER_HOST=sagiri
 NGINX_SITES=/etc/nginx/conf.d
+LOG_DIR=/var/log/nginx
 
-docker service create --name=nginx --mount="type=bind,src=$NGINX_SITES,dst=$NGINX_SITES" --replicas=1 --constraint="node.hostname == $DOCKER_HOST" --network=danboorunet --publish="80:80" --detach=true nginx
+docker service create --name=nginx --mount="type=bind,src=$NGINX_SITES,dst=$NGINX_SITES" --mount="type=bind,src=$LOG_DIR,dst=$LOG_DIR" --replicas=1 --constraint="node.hostname == $DOCKER_HOST" --network=danboorunet --publish="80:80" --detach=true nginx
