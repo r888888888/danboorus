cae529b64aa0af67fc4c7b9109485cc9ab2829d9
Albert Yi <ayi@desk.com>
2017-10-30 16:37:53 -0700

    enable ssl

--- script/setup/start_letsencrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DOCKER_HOST=sagiri
-NGINX_SSL_CERTS=/etc/nginx/ssl
-LOG_DIR=/var/log/nginx
-
-docker service create --name=letsencrypt --mount="type=bind,src=$NGINX_SSL_CERTS,dst=$NGINX_SSL_CERTS" --replicas=1 --constraint="node.hostname == $DOCKER_HOST" --network=danboorunet --detach=true jrcs/letsencrypt-nginx-proxy-companion
--- script/setup/start_nginx.sh
+++ script/setup/start_nginx.sh
@@ -2,8 +2,8 @@
 
 DOCKER_HOST=sagiri
 NGINX_SITES=/etc/nginx/conf.d
-NGINX_SSL_CERTS=/etc/nginx/ssl
+NGINX_SSL=/etc/nginx/ssl
 LOG_DIR=/var/log/nginx
 CACHE_DIR=/tmp/nginx-cache
 
-docker service create --name=nginx --mount="type=bind,src=$NGINX_SSL_CERTS,dst=$NGINX_SSL_CERTS" --mount="type=bind,src=$NGINX_SITES,dst=$NGINX_SITES" --mount="type=bind,src=$LOG_DIR,dst=$LOG_DIR" --mount="type=bind,src=$CACHE_DIR,dst=$CACHE_DIR" --replicas=1 --constraint="node.hostname == $DOCKER_HOST" --network=danboorunet --publish="80:80" --publish="443:443" --detach=true nginx
+docker service create --name=nginx --mount="type=bind,src=$NGINX_SSL,dst=$NGINX_SSL" --mount="type=bind,src=$NGINX_SITES,dst=$NGINX_SITES" --mount="type=bind,src=$LOG_DIR,dst=$LOG_DIR" --mount="type=bind,src=$CACHE_DIR,dst=$CACHE_DIR" --replicas=1 --constraint="node.hostname == $DOCKER_HOST" --network=danboorunet --publish="80:80" --publish="443:443" --detach=true nginx
